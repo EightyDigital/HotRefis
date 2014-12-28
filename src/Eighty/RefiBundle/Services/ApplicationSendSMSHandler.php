2bb0e242ff3b95fd6ac64c2e73c31c9ee57e2d57
Jay Jamero <jayjamero@MacBook-Pro.myrepublic.com.sg>
2014-12-29 04:47:49 +0800

    changed order of variables

--- src/Eighty/RefiBundle/Services/ApplicationSendSMSHandler.php
+++ src/Eighty/RefiBundle/Services/ApplicationSendSMSHandler.php
@@ -7,22 +7,6 @@ class ApplicationSendSMSHandler
 	public function sendSMS($report_hashed_url, $transactionId, $amicus_person_id)
 	{
 		sleep(3);
-		
-		// generate tiny url from google
-		$c = curl_init();
-		curl_setopt($c, CURLOPT_URL, "https://www.googleapis.com/urlshortener/v1/url");
-		curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($c, CURLOPT_TIMEOUT, 10);
-		curl_setopt($c, CURLOPT_POST, true);
-		curl_setopt($c, CURLOPT_POSTFIELDS, '{"longUrl": "'.$report_hashed_url.'"}'); 
-		curl_setopt($c, CURLOPT_HTTPHEADER, $headers);
-
-		$response = curl_exec($c); 
-		curl_close($c);
-		
-		$report_tiny_url = json_decode($response);
-		
 		// send SMS request
 		$soapUrl = "http://sms.dncfilter.com/SMS.asmx";
         $xml_post_string = '<?xml version="1.0" encoding="utf-8"?>
@@ -39,13 +23,29 @@ class ApplicationSendSMSHandler
 								  <senderNumber>82015620</senderNumber>
 								</sendSMS>
 							  </soap:Body>
-							</soap:Envelope>';   
+							</soap:Envelope>';
 
 		$headers = array(
 			"Content-type: text/xml;charset=\"utf-8\"",
-			"SOAPAction: http://tempuri.org/sendSMS", 
+			"SOAPAction: http://tempuri.org/sendSMS",
 			"Content-length: ".strlen($xml_post_string),
-		); 
+		);
+		// generate tiny url from google
+		$c = curl_init();
+		curl_setopt($c, CURLOPT_URL, "https://www.googleapis.com/urlshortener/v1/url");
+		curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($c, CURLOPT_TIMEOUT, 10);
+		curl_setopt($c, CURLOPT_POST, true);
+		curl_setopt($c, CURLOPT_POSTFIELDS, '{"longUrl": "'.$report_hashed_url.'"}');
+		curl_setopt($c, CURLOPT_HTTPHEADER, $headers);
+
+		$response = curl_exec($c);
+		curl_close($c);
+
+		$report_tiny_url = json_decode($response);
+
+
 
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $soapUrl);
@@ -55,9 +55,9 @@ class ApplicationSendSMSHandler
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $xml_post_string);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-		$response = curl_exec($ch); 
+		$response = curl_exec($ch);
 		curl_close($ch);
-		
+
 		return true;
 	}
 
